ca88f78087f7ac282c37c128351014fb2e47405c
Juan Carlos Olivares Rojas <juan.or@morelia.tecnm.mx>
2025-03-23 16:12:42 -0600

    Agregando ultimos cambios

--- js/procesar.js
+++ js/procesar.js
@@ -17,5 +17,39 @@ $(document).ready(function(){
         //$("#tabla").fadeIn(4000)   
         $("#tabla").fadeToggle(4000)      
     })
+
+    $("h1").mouseover(function(){
+        
+        let contenido = $("#correo").val()
+        $("#principal").after("<h2>"+contenido+"</h2>")
+    })
+
+    //$("#correo").keypress(function(){
+    $("#correo").keyup(function(){
+        
+        let texto = $(this).val()
+        $("#titulo").text(texto)
+    })
+
+    $("input[type=range]").change(function(){
+        
+        let rojo = $("#r").val()
+        let verde = $("#g").val()
+        let azul = $("#b").val()
+
+        let c = rojo+","+verde+","+azul
+        let color = "rgb("+c+")"
+
+        //rojo.toHex()
+        rojo = parseInt(rojo, 16)
+        $("#correo").val(rojo)
+
+        $("#cuadro").css("background-color", color)
+    })
+
+    $("#subtitulo").dblclick(function(){
+        //alert("Entrando")
+        $("#x").load("cajas.html")
+    })
     
 })
